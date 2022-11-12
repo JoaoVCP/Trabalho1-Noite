d2b5faad058696876cffb7d8905c35f27dfbc769
JoaoVCP <jvcallera@gmail.com>
2022-11-11 21:37:38 -0300

    Melhorando layout 2

--- index.php
+++ index.php
@@ -23,45 +23,45 @@ if( isset ($_POST) && !empty($_POST) ){
 
 <div class="card" style="width: 18rem;">
   <div class="card-header">
-<label>Pergunta</label><br>
-<textarea name="pergunta"></textarea>
+    <label><h3>Questão</h3></label><br>
+    <textarea name="pergunta" style="border-radius: 5px 5px 5px 5px" placeholder="Insira o enunciado da questão"></textarea>
 
-<br><br>
+    <br><br>
 
-<label>A)</label>
-<input input class="form-check-input" type="radio" name="correta" value="A" />
-<input type="text" name="A" style="border-radius: 5px 5px 5px 5px"/>
+    <label>A)</label>
+    <input input class="form-check-input" type="radio" name="correta" value="A"/>
+    <input type="text" name="A" style="border-radius: 5px 5px 5px 5px" placeholder="Insira o texto da opção A"/>
 
-<br><br>
+    <br><br>
 
-<label>B)</label>
-<input input class="form-check-input" type="radio" name="correta" value="B" />
-<input type="text" name="B" style="border-radius: 5px 5px 5px 5px"/>
+    <label>B)</label>
+    <input input class="form-check-input" type="radio" name="correta" value="B"/>
+    <input type="text" name="B" style="border-radius: 5px 5px 5px 5px" placeholder="Insira o texto da opção B"/>
 
-<br><br>
+    <br><br>
 
-<label>C)</label>
-<input input class="form-check-input" type="radio" name="correta" value="C" />
-<input type="text" name="C" style="border-radius: 5px 5px 5px 5px"/>
+    <label>C)</label>
+    <input input class="form-check-input" type="radio" name="correta" value="C" />
+    <input type="text" name="C" style="border-radius: 5px 5px 5px 5px" placeholder="Insira o texto da opção C"/>
 
-<br><br>
+    <br><br>
 
-<label>D)</label>
-<input input class="form-check-input" type="radio" name="correta" value="D" />
-<input type="text" name="D" style="border-radius: 5px 5px 5px 5px"/>
+    <label>D)</label>
+    <input input class="form-check-input" type="radio" name="correta" value="D" />
+    <input type="text" name="D" style="border-radius: 5px 5px 5px 5px" placeholder="Insira o texto da opção D"/>
 
-<br><br>
+    <br><br>
 
-<label>E)</label>
-<input input class="form-check-input" type="radio" name="correta" value="E" />
-<input type="text" name="E" style="border-radius: 5px 5px 5px 5px"/>
+    <label>E)</label>
+    <input input class="form-check-input" type="radio" name="correta" value="E" />
+    <input type="text" name="E" style="border-radius: 5px 5px 5px 5px" placeholder="Insira o texto da opção E"/>
 
-<br><br>
+    <br><br>
 
-<button type="button" class="btn btn-success">Salvar Pergunta</button>
+    <button type="button" class="btn btn-success">Salvar Pergunta</button>
 
-</form>
-</div>
+    </form>
+    </div>
 </div>
 
 <br>
